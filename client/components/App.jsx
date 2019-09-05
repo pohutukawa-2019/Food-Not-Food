0aa8d9d81f8f42e2ee8dd2c86171a4a02f9a936a
Don Smith <don@antiantonym.com>
2019-09-06 11:26:39 +1200

    add AnswerModal

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
 import foodNotFood from '../foodNotFood'
-
-// import Results from './Results'
 
 export default class App extends React.Component {
 
   state = {
-      score = 5,
-      currentItem = foodNotFood[0]
+    score: 5,
+    isAnswerModalVisible: false,
+    currentItem: foodNotFood[0]
+  }
+
+  handleModalClose = () => {
+    this.setState({
+      isAnswerModalVisible: false
+    })
   }
 
   render () {
     return (
-      <React.Fragment>
-        <div className="SOME-NICE-CSS">
-
-        </div>
-      </React.Fragment>
+      <>
+        
+        <AnswerModal
+          isVisible={this.state.isAnswerModalVisible}
+          handleModalClose={this.handleModalClose} />
+      </>
     )
   }
 }
