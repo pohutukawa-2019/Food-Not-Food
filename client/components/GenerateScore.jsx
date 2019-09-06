2beb300e10488822b4a9aeb4b30a878d82255f9b
JamesS <jamessanday@gmail.com>
2019-09-06 13:30:46 +1200

    test

--- client/components/GenerateScore.jsx
+++ client/components/GenerateScore.jsx
@@ -1,7 +1,56 @@
+import React, { Component } from 'react'
+
+const health = 5
+
+export class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      health: 5
+    } 
+    this.handleNext = this.handleNext.bind(this)
+  }
+
+componentDidMount() {
+  this.getHealth()
+}
+
+function clickedNext(this.state.foodNOTfood) {
+  getHealth()
+}
+
+// getResult() {
+//   getHealth
+// }
+
+function getHealth(fNotf) {
+  if (fNOTf.food) {
+    this.state.health = this.state.health + 1
+  } else {
+    this.state.health = this.state.health - 1
+  }
+}
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default GenerateScore
+
+
+
+var health = 5
+
+const foodNOTfood = require('../foodNotFood')
 ////// 1
 ///////////
 // Create New Array. 
-
+// console.log(foodNOTfood)
 // EAT GOOD = 2
 // DON'T EAT = 1
 // EAT BAD = 0
@@ -15,6 +64,7 @@
 //////2
 /////
 // Base Score is 5
+
 /// If yes true and JSON true, i = score + 1
 /// If yes true and JSON false i = score - 1
 /// If no false and JSON false, i = score 
