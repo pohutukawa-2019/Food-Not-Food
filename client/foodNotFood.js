6cd56884e35175702c53d5d203cb63e9c23b2eb4
Iris Riddell <riddell.iris@gmail.com>
2019-09-06 13:51:46 +1200

    added some CSS and name properties to foodNotFood.js

--- client/foodNotFood.js
+++ client/foodNotFood.js
@@ -11,12 +11,14 @@ module.exports = [
  {
    // TREE COSTUME 
    id: 1,
+   name: 'a tree costume',
    food: false,
    img: 'https://images-na.ssl-images-amazon.com/images/I/81kscOVp6NL._SY450_.jpg'
  },
  {
    // Vape PEN
   id: 2,
+  name: 'a vape pen',
   food: false,
   img: 'https://vaping.com/media/catalog/product/cache/1/image/1500x1500/9df78eab33525d08d6e5fb8d27136e95/s/m/smok-stick-prince_1.png'
  },
@@ -24,48 +26,56 @@ module.exports = [
 
   // Blue CHEESE
   id: 3,
+  name: 'blue cheese',
   food: true,
   img: 'https://i0.wp.com/osteperler.no/wp-content/uploads/2013/10/Persille-de-malzieu-closeup-4.jpg?fit=640%2C425&ssl=1'
  },
  {
   // BAMBOO TAKENOKO
  id: 4,
+ name: 'takenoko',
  food: true,
  img: 'https://wellnessoptions.ca/assets/image-cache/bamboo%20shoot%20prevent%20weight%20gain.c24799e8.jpg'
 },
 {
 // CRACKED HEELS
   id: 5,
+  name: 'stanky feet',
   food: false,
   img: 'https://www.triadfoot.com/wp-content/uploads/2016/03/iStock_000083370501_Full-SMALLER-735x1024.jpg'
  },
  {
   // hair on floor',
   id: 6,
+  name: 'hair',
   food: false,
   img: 'https://i.pinimg.com/originals/9f/4b/84/9f4b84066cbfe29e64bbaa24e3ba5bae.jpg'
  },
  {
   // Magic Mushroom
   id: 7,
+  name: 'magic mushrooms',
   food: false,
   img: 'https://img.buzzfeed.com/buzzfeed-static/static/2019-01/14/21/asset/buzzfeed-prod-web-03/sub-buzz-25788-1547518553-1.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto&output-quality=auto&output-format=auto&downsize=360:*'
  },
  {
    // WOOLEN JUMPER 
   id: 8,
+  name: 'a woollen jumper',
   food: false,
   img: 'https://www.rokit.co.uk/media/catalog/product/cache/small_image/360x480/beff4985b56e3afdbeabfc89641a4582/W/O/WOCKNI002678_CAM__2511.JPG'
  },
  {
    // PUMPKIN BREAD
   id: 9,
+  name: 'pumpkin bread',
   food: true,
   img: 'https://tmbidigitalassetsazure.blob.core.windows.net/secure/RMS/attachments/37/1200x1200/exps179268_TH143194D06_19_2b.jpg'
  },
  {
    // SICHUAN PEPPER
   id: 10,
+  name: 'Sichuan pepper',
   food: true,
   img: 'https://worldflavorz.com/wp-content/uploads/2013/04/Szechuan-Peppers-main.jpg'
  }
